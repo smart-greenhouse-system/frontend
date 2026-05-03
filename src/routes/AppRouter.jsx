5ee73fac0af8baaf0cbc7ed780af4b766be53b91
Your Name <you@example.com>
2026-05-03 17:00:29 -0500

    Esta es la vista de control de actuadores

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -10,6 +10,7 @@ import ResourceConsumption from "../modules/inventory/pages/ResourceConsumption"
 import Reports from "../modules/inventory/pages/Reports";
 import DashboardLayout from "../modules/layout/DashboardLayout";
 import MonitoreoIoT from "../modules/monitoreo/MonitoreoIoT";
+import Control from "../modules/control/Control";
 
 const AppRouter = () => {
   return (
@@ -29,6 +30,7 @@ const AppRouter = () => {
           <Route path="/inventory/consumption" element={<ResourceConsumption />} />
           <Route path="/inventory/reports" element={<Reports />} />
           <Route path="/monitoreo" element={<MonitoreoIoT />} />
+          <Route path="/control" element={<Control />} />
         </Route>
 
         {/* Redirección por defecto */}
